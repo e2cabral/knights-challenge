b33df7796702bf9b73fbb19956ffc6ce626fae68
Edson Cabral Junior <k2.edsonc@grupofluery.com.br>
2023-03-18 19:03:12 -0300

    chore: create a weapon data mapping

---
+++ src/domain/entities/weapon.entity.ts
@@ -0,0 +1,12 @@
+import { Schema } from 'mongoose'
+import * as mongoose from 'mongoose'
+
+const Weapon = new Schema({
+  _id: { type: Schema.Types.ObjectId },
+  name: { type: String, required: true },
+  mod: { type: Number, required: true },
+  attr: { type: String, required: true },
+  knights: [{ type: Schema.Types.ObjectId, required: false }]
+}, { collection: 'weapons' })
+
+export const Weapons = mongoose.model('Weapon', Weapon)
