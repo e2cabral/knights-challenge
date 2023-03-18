c6dc79367bec2617776f9b04080e4b7cc819add3
Edson Cabral Junior <k2.edsonc@grupofluery.com.br>
2023-03-18 19:11:04 -0300

    feat: create the schema validator to the body in the creation of a knight

---
+++ src/handlers/create/schema.validator.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi'
+
+export const validator = Joi.object({
+  name: Joi.string().required(),
+  nickname: Joi.string().required(),
+  birthday: Joi.string().required(),
+  class: Joi.string().required(),
+  weapons: Joi.array(),
+  equippedWeapon: Joi.string().optional(),
+  attributes: Joi.object().required(),
+  keyAttribute: Joi.string().required()
+})
