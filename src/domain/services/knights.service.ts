2a23c79a59f3669edefedfb225eeca0560af1add
Edson Cabral Junior <k2.edsonc@grupofluery.com.br>
2023-03-18 19:08:58 -0300

    feat: create the knights service

---
+++ src/domain/services/knights.service.ts
@@ -0,0 +1,32 @@
+import type Knight from '../models/knight.model'
+import KnightsRepositories from '../../data/repositories/knights.repositories'
+import { isNullOrUndefined } from '../../infra/helpers/verification.helper'
+
+export class KnightsService {
+  async find (
+    page: string,
+    itemsPerPage: string,
+    filter?: string
+  ): Promise<Knight[] | Error> {
+    try {
+      const repository = new KnightsRepositories()
+
+      if (isNullOrUndefined(filter)) {
+        return await repository.find(page, itemsPerPage)
+      }
+
+      return await repository.find(page, itemsPerPage, filter)
+    } catch (err) {
+      return new Error((err as Error).message)
+    }
+  }
+
+  async create (knight: Knight): Promise<void> {
+    try {
+      const repository = new KnightsRepositories()
+      await repository.create(knight)
+    } catch (err) {
+      throw new Error((err as Error).message)
+    }
+  }
+}
