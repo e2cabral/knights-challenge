b34fc8e5353de33723cfff519b568e3ab7e1b386
Edson Cabral Junior <k2.edsonc@grupofluery.com.br>
2023-03-19 14:34:27 -0300

    feat: add an update knight method

--- src/handlers/update/app.ts
+++ src/handlers/update/app.ts
@@ -1,6 +1,28 @@
 import { type APIGatewayEvent, type APIGatewayProxyResult } from 'aws-lambda'
-import { Ok } from '../../infra/helpers/http.helper'
+import { BadRequest, MalformedObject, Ok } from '../../infra/helpers/http.helper'
+import { routeParamValidator, validator } from './schema.validator'
+import { isNullOrUndefined } from '../../infra/helpers/verification.helper'
+import { KnightsService } from '../../domain/services/knights.service'
+import Knight from '../../domain/models/knight.model'
 
 export const handle = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-  return Ok(200, [])
+  const knight = JSON.parse((event.body != null) ? event.body : '{}') as Knight
+  const params = event.pathParameters as { id: string }
+
+  const isValid = validator.validate(knight)
+  const isIdNotEmpty = routeParamValidator.validate(params)
+
+  if (isNullOrUndefined(isValid) || isNullOrUndefined(isIdNotEmpty)) {
+    return MalformedObject()
+  }
+
+  try {
+    const service = new KnightsService()
+
+    await service.update(knight, params.id)
+
+    return Ok(200, 'Knight successfully updated!')
+  } catch (err) {
+    return BadRequest((err as Error))
+  }
 }
