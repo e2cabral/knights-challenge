8ce97d393c427277e2a596cd45624d3c20e3b179
Edson Cabral Junior <k2.edsonc@grupofluery.com.br>
2023-03-19 15:39:00 -0300

    feat: add a get by id method

--- src/handlers/get-by-id/app.ts
+++ src/handlers/get-by-id/app.ts
@@ -1,6 +1,23 @@
 import { type APIGatewayEvent, type APIGatewayProxyResult } from 'aws-lambda'
-import { Ok } from '../../infra/helpers/http.helper'
+import { BadRequest, MalformedObject, Ok } from '../../infra/helpers/http.helper'
+import { routeParamValidator } from './schema.validator'
+import { isNullOrUndefined } from '../../infra/helpers/verification.helper'
+import { KnightsService } from '../../domain/services/knights.service'
 
 export const handle = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-  return Ok(200, [])
+  try {
+    const params = event.pathParameters as { id: string }
+    const isIdNotEmpty = routeParamValidator.validate(params)
+
+    if (isNullOrUndefined(isIdNotEmpty)) {
+      return MalformedObject()
+    }
+
+    const service = new KnightsService()
+    const data = await service.findById(params.id)
+
+    return Ok(200, data)
+  } catch (err) {
+    return BadRequest((err as Error))
+  }
 }
