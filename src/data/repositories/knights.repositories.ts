ce2c0b1a139690842deb7cf3ee6ace3711df144b
Edson Cabral Junior <k2.edsonc@grupofluery.com.br>
2023-03-19 14:32:35 -0300

    chore: create an update method into the repository

--- src/data/repositories/knights.repositories.ts
+++ src/data/repositories/knights.repositories.ts
@@ -3,6 +3,7 @@ import { Connect } from '../../main/config/database.config'
 import { Knights } from '../../domain/entities/knights.entity'
 import { Types } from 'mongoose'
 import { Weapons } from '../../domain/entities/weapon.entity'
+import { isNullOrUndefined } from '../../infra/helpers/verification.helper'
 
 export default class KnightsRepositories {
   async find (
@@ -44,6 +45,36 @@ export default class KnightsRepositories {
 
       for (const id of data.weapons) {
         await Weapons.updateOne({ _id: id }, { $push: { knights: data._id } })
+      }
+    } catch (err) {
+      throw new Error((err as Error).message)
+    }
+  }
+
+  async update (knight: Knight, id: string): Promise<void> {
+    await Connect()
+
+    try {
+      const set = {
+        $set: {
+          name: knight.name,
+          birthday: knight.birthday,
+          nickname: knight.nickname,
+          attributes: knight.attributes,
+          keyAttribute: knight.keyAttribute,
+          class: knight.class,
+          weapons: knight.weapons,
+          equippedWeapon: knight.equippedWeapon
+        }
+      }
+      const data = await Knights.findOneAndUpdate({ _id: id }, set, { upsert: true })
+
+      if (!data) {
+        throw new Error('You must provide a valid id')
+      }
+
+      for (const weaponId of data.weapons) {
+        await Weapons.updateOne({ _id: weaponId }, { $push: { knights: id } })
       }
     } catch (err) {
       throw new Error((err as Error).message)
