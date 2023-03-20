71aa7ec05f12cc96b410076cd2d4e0c6dbfb273c
Edson Cabral Junior <k2.edsonc@grupofluery.com.br>
2023-03-20 12:12:14 -0300

    refactor: move to a separated folder

--- src/handlers/weapons/create/app.ts
+++ src/handlers/weapons/create/app.ts
@@ -1,14 +1,17 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { validator } from './schema.validator'
-import { Weapons } from '../../domain/entities/weapon.entity'
-import { isNullOrUndefined } from '../../infra/helpers/verification.helper'
-import { BadRequest, MalformedObject, Ok } from '../../infra/helpers/http.helper'
+import { Weapons } from '../../../domain/entities/weapon.entity'
+import { isNullOrUndefined } from '../../../infra/helpers/verification.helper'
+import { BadRequest, MalformedObject, Ok } from '../../../infra/helpers/http.helper'
+import { Connect } from '../../../main/config/database.config'
 
 // This method was created just to create new Weapons,
 // so it won't follow any best practices
 // I hope you guys understand
 export const handle = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   try {
+    await Connect()
+
     const weapon = JSON.parse(event.body ? event.body : '{}')
     const isValid = validator.validate(weapon).error
 
